ae4f31597b4832ac7793d6f95fbc73207db1932e
Ryan Mills <ranemills@gmail.com>
2017-02-25 22:34:40 +0000

    Add start/stop timer functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-angular.module('timeTrackerApp');
+angular.module('timeTrackerApp', ['angularMoment'])
+.controller('timerCtrl', function($scope, $interval, moment) {
+
+  $scope.time = moment();
+  $scope.isTiming = false;
+
+  function setTimeAgo() {
+    $scope.timeAgo = moment.utc(moment().diff($scope.startTime));
+  }
+
+
+  $scope.start = function() {
+    $scope.startTime = moment();
+    $scope.isTiming = true;
+
+    $scope.interval = $interval(setTimeAgo, 1000);
+  };
+
+  $scope.stop = function() {
+    $scope.duration = moment().diff($scope.startTime);
+    console.log($scope.duration);
+    $interval.cancel($scope.interval);
+
+    $scope.isTiming = false;
+  }
+
+});
